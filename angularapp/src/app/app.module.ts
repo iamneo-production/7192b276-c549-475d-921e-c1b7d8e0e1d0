9c61b25a10903bb39b81d0e7b6d8d1c75f3388cb
UshaShreeSyreddy <ushasree054@gmail.com>
2021-09-30 11:32:59 +0000

    Commit-Login-Signup

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    SignupPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
