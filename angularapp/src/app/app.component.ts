e6ffa03acd29e33f70130b52ddb240f14ee31e40
adityaVelhal <aditya.v.velhal@gmail.com>
2021-10-02 01:55:59 +0530

    Resolved conflicts

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<app-login-page></app-login-page>'
-})
-export class AppComponent {
-  title = 'Login';
-}
-=======
 import { Component } from '@angular/core';
 
 @Component({
@@ -19,4 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularapp';
 }
->>>>>>> main
