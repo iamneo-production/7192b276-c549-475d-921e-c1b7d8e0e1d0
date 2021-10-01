ee05b13584c511a2d081d75cafe456bf3fb5d8a0
UshaShreeSyreddy <ushasree054@gmail.com>
2021-10-01 16:44:30 +0000

    committingComponents

--- angularapp/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<app-login-page></app-login-page>'
-})
-export class AppComponent {
-  title = 'Login';
-}
