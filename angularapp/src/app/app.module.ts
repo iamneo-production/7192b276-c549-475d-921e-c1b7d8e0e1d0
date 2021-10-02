813fc8c4fd80d3b3acc7853e654e8a32bdd877bc
himanshudeshmukh <d.himanshudeshmukh@gmail.com>
2021-10-02 11:52:00 +0000

    navbar changed

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CredentialLockerComponent } from './credential-locker/credential-locker.component';
-import { CheckingComponentComponent } from './checking-component/checking-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CredentialLockerComponent,
-    CheckingComponentComponent
   ],
   imports: [
     BrowserModule,
