39133284c4d41d6fdd3b3642172626c820a74829
himanshudeshmukh <d.himanshudeshmukh@gmail.com>
2021-10-13 04:41:41 +0000

    .

--- springapp/src/main/java/com/examly/springapp/controller/SignupController.java
+++ springapp/src/main/java/com/examly/springapp/controller/SignupController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
- @CrossOrigin("https://8080-ddebdbcdadaeefefcfbefdaaebbaacaca.examlyiopb.examly.io/")
+ @CrossOrigin("https://8080-ddebdbcdadaeefefcfbefdaaebbaacaca.examlyiopb.examly.io")
 public class SignupController {
 
 
