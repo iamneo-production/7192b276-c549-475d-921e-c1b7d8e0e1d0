622c8135b8769a4f771fc62f829ae855744f46c6
UshaShreeSyreddy <ushasree054@gmail.com>
2021-10-18 05:28:59 +0000

    CredentialsVault

---
+++ springapp/src/main/java/com/examly/springapp/controller/SignupController.java
@@ -0,0 +1,58 @@
+package com.examly.springapp.controller;
+
+
+
+import com.examly.springapp.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.examly.springapp.service.UserService;
+
+@RestController
+// @CrossOrigin("*")
+public class SignupController {
+
+
+    @Autowired
+    private UserService service;
+
+    
+    @PostMapping ( "/signup")
+    @CrossOrigin(origins = "https://8081-ddebdbcdadaeefefcfbefdaaebbaacaca.examlyiopb.examly.io/signup")
+    public UserModel saveUser(@RequestBody  UserModel user) throws Exception
+    {
+        String  tempEmailId = user.getEmail();
+
+        if(tempEmailId !=null &&  !"".equals(tempEmailId))
+        {
+            UserModel userObj = service.fetchUserByEmail(tempEmailId);
+            if(userObj !=null)
+            {
+                throw  new Exception("already present");
+            }
+        }
+        UserModel userObj = null;
+        userObj = service.saveUserDetails(user);
+        return userObj;
+    }
+    
+
+    @PostMapping ( "/login")
+    @CrossOrigin(origins = "https://8081-ddebdbcdadaeefefcfbefdaaebbaacaca.examlyiopb.examly.io/login")
+    public UserModel loginUser(@RequestBody UserModel user) throws Exception
+    {
+        String tempEmailId = user.getEmail();
+        String tempPass = user.getPassword();
+        UserModel userObj = null;
+        if(tempEmailId !=null && tempPass !=null)
+        {
+           userObj =  service.fetchUserByEmailAndPassword(tempEmailId , tempPass);
+        }
+
+        if(userObj ==null)
+        {
+            throw new Exception("does not exist");
+        }
+        return userObj;
+    }
+}
+
